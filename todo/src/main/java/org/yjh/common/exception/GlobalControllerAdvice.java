f3b911f2c2c0f9a96d92acb60403005436ee36d3
jeongjihun <wlgns2234@naver.com>
2024-11-21 05:07:51 +0900

    feat: 할 일 상태 생성 기능 구현

--- todo/src/main/java/org/yjh/common/exception/GlobalControllerAdvice.java
+++ todo/src/main/java/org/yjh/common/exception/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package org.yjh.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -29,9 +31,23 @@ public class GlobalControllerAdvice {
                 .toList();
     }
 
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+        return e.getMessage();
+    }
+
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
     public String handleNoSuchElementException(NoSuchElementException e) {
+        log.error(e.getMessage());
+        return e.getMessage();
+    }
+
+    @ResponseStatus(CONFLICT)
+    @ExceptionHandler(BaseException.class)
+    public String handleBaseException(BaseException e) {
         log.error(e.getMessage());
         return e.getMessage();
     }
--- todo/src/main/java/org/yjh/task/domain/TaskStatus.java
+++ todo/src/main/java/org/yjh/task/domain/TaskStatus.java
@@ -1,9 +1,20 @@
 package org.yjh.task.domain;
 
+import org.yjh.common.exception.BaseException;
+
+import java.util.Arrays;
+
 public enum TaskStatus {
     TODO,
     IN_PROGRESS,
     ON_HOLD,
     COMPLETED,
-    CANCELLED
+    CANCELLED;
+
+    public static TaskStatus createBy(String status) {
+        return Arrays.stream(TaskStatus.values())
+                .filter(taskStatus -> taskStatus.name().equalsIgnoreCase(status))
+                .findFirst()
+                .orElseThrow(() -> new BaseException(String.format("unsupported task status : %s", status)));
+    }
 }
