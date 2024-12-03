f67601a789b6424b5390f3a92af1cd20ef5419a0
jeongjihun <wlgns2234@naver.com>
2024-12-04 03:25:48 +0900

    feat: 트랜잭션 적용

--- board/src/main/java/yjh/board/member/application/MemberService.java
+++ board/src/main/java/yjh/board/member/application/MemberService.java
@@ -2,6 +2,7 @@ package yjh.board.member.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import yjh.board.member.application.port.MemberRepository;
 import yjh.board.member.domain.Member;
 import yjh.board.member.domain.MemberCreate;
@@ -9,23 +10,26 @@ import yjh.board.member.domain.MemberCreate;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Transactional
     public Member save(MemberCreate memberCreate) {
         var member = Member.create(memberCreate);
         return memberRepository.save(member);
     }
 
-    public List<Member> findAll() {
-        return memberRepository.findAll();
-    }
-
+    @Transactional
     public void delete(Long id) {
         var member = findById(id);
         memberRepository.delete(member.getId());
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
     }
 
     public Member findById(Long id) {
